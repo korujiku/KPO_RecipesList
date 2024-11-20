c23fbdb4cbe3331235f95e94154dd1390e44d307
Анастасия Акимова <akimova.261095@gmail.com>
2024-11-20 20:34:23 +0400

    небольшие фиксы + полупустые странички

--- src/main/java/com/example/RecipesList/RecipesListApplication.java
+++ src/main/java/com/example/RecipesList/RecipesListApplication.java
@@ -2,11 +2,14 @@ package com.example.RecipesList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RecipesListApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RecipesListApplication.class, args);
 	}
 
