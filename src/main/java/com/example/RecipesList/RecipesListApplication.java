87fa1d3d0c8a59a54d98d57c2bc40900581544e5
Анастасия Акимова <akimova.261095@gmail.com>
2024-11-20 15:47:21 +0400

    Инициализация проекта

---
+++ src/main/java/com/example/RecipesList/RecipesListApplication.java
@@ -0,0 +1,13 @@
+package com.example.RecipesList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RecipesList/RecipesListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RecipesList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
